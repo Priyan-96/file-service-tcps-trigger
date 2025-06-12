d51e40b8114ec7b37b3c11ac09c8b7c47c8ffb54
tspriyan-96 <shanmugapriyan_t@trimble.com>
2025-06-12 19:44:54 +0530

    Correct implementation of TCPLT-12581

---
+++ file-service-tcps-trigger/functions/FolderDeleteRevisionFunction/folder_delete_revision_fn.py
@@ -0,0 +1,56 @@
+"""
+This module consists of the lambda handler function.
+The Lambda function handler is the method in your function code that processes events.
+"""
+import json
+import logging as log
+from typing import Dict
+
+import pyckson
+import os
+import requests
+
+from model.folder_event import FolderDeleteQueueMessage
+from folder_delete_revision_service import FolderDeleteRevisionService
+from utils import logging_utils
+
+# x-ray tracing configuration
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+
+patch(('requests',))
+
+folder_delete_revision_service = FolderDeleteRevisionService()
+
+def handler(event, context):
+    """
+    When your function is invoked, Lambda runs the handler method.
+    When the handler exits or returns a response, it becomes available to handle another event.
+    @param event: sqs event
+    @param context: consists of lambda context information like requestId
+    """
+    # Get AWS lambda request id
+    request_id = context.aws_request_id
+    # Update logger with awsRequestId and json formatting
+    logging_utils.update_logger_format(request_id=request_id)
+    log.info(f'Received message(s): {event} from SQS to process')
+    try:
+        # Perform Folder Delete Revision on each record sequentially
+        for record in event['Records']:
+            # Convert sns event string to dict
+            sns_event: Dict = json.loads(record['body'])
+            # Get folder event string from sns message
+            folder_event_str: str = sns_event['Message']
+            log.info(f'Received Message event {folder_event_str}')
+            # Convert the folder event string to FolderDeleteQueueMessage object
+            folder_event: FolderDeleteQueueMessage = pyckson.parse(FolderDeleteQueueMessage, json.loads(folder_event_str))
+            # Update log
+            log.info(f'Received event {folder_event}')
+            
+            folder_delete_revision_service.invoke_tcps_api(folder_event)
+    except Exception as e:
+        log.error(f'Exception while processing the folder event {e}')
+        # throw the exception so that lambda understands that the message is not processed successfully
+        raise e 
+    
+
